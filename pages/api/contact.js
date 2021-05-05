e4fa005100c7b33269d843df193b26c87d01531a
MirkoCulibrk <micoculibrk@gmail.com>
2021-05-05 02:28:07 +0200

    Test

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,8 +4,8 @@ export default async (req, res) => {
   const {subject,name,email,message}=req.body;
   const transporter=nodemailer.createTransport({
     host:'smtp.gmail.com',
-    port:465,
-    secure:true,
+    port:587,
+    secure:false,
     auth:{
       user:process.env.USER,
       pass:process.env.PASS
